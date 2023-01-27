58c4f6cb8f2dd33b64b2585df410a8741fbedd3e
Cooper Lawson <CSLawson@lcs.on.ca>
2023-01-27 14:14:01 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Cooper Lawson on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
