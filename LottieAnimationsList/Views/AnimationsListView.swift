f19874f39f89eb3452283f1db2847a00dd61b19f
Cooper Lawson <CSLawson@lcs.on.ca>
2023-01-27 14:17:00 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by Cooper Lawson on 2023-01-27.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text("They Call Me C-Law")
+    }
+}
+
+struct AnimationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsListView()
+    }
+}
